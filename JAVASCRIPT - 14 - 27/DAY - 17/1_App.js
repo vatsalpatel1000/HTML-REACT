d959e6843b5969a1f53c367ce9af10449925aecf
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-10 00:04:28 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 17/1_App.js
+++ JAVASCRIPT - 14 - 27/DAY - 17/1_App.js
@@ -132,3 +132,33 @@ for(fruit of fruit4){
     console.log(heros);
 }
 }
+
+/* Practice Question */
+let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+let num = 2;
+    for(i=0; i<arr.length; i++){
+        if(arr[i] == num){
+            arr.splice(i,1);
+        }   
+    }
+
+
+let number = 287152; 
+let count = 0;
+let copy = number;
+    while(copy>0){
+        count++;
+        copy = Math.floor(copy/10);
+    }
+    console.log(count);
+
+
+let number1 = 287152; 
+let sum = 0;
+let copy1 = number;
+    while(copy1>0){
+        digit = copy1 % 10;
+        sum+= digit;
+        copy1 = Math.floor(copy1/10);
+    }
+    console.log(sum);    
