33ec09fd827689d874b1f800b67c644227b04293
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-12 03:08:56 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 18/App.js
+++ JAVASCRIPT - 14 - 27/DAY - 18/App.js
@@ -1,5 +1,18 @@
-let student  = {
-    name : "vatsal",
+const student  = {                /* Object literals  it will show object with key & their values  and always create const  */
+    name : " ",
     age: 23,
     marks : 75.5
 }
+
+let student1  = ["vatsal",23,75.5];                  /* Array  it will show only values */
+
+const instagram = {
+    username : '@vatsalpatel1000',
+    content : '#first post',
+    likes : 150,
+    report : 5,
+    tags : '@apnacollege'
+};
+
+// instagram['username']; OR                            Get object literal value
+// instagram.username;
