ec3e207bcc8f0cc41fefb46c4e9011e7603acf43
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-14 00:11:49 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 19/App.js
+++ JAVASCRIPT - 14 - 27/DAY - 19/App.js
@@ -30,7 +30,7 @@ console.log(dice);
 }
 rolldice();
 
-
+// Function with argument
 function details(name,age){
     console.log(name + ' and  age is ' + age)
 }
@@ -57,7 +57,7 @@ function table(n){
 }
 table(2);
 
-
+// Return leyword
 function sum1(a,b){
     console.log('hello');
     console.log('hello');
@@ -79,3 +79,127 @@ function getsum(n){
 getsum(n);
 
 
+
+let str4 =['hi','hello','bye','!'];
+    function concat(str4){
+        let result = '';
+        for(let i = 0; i<str4.length; i++){ 
+            result = result + str4[i];
+        }
+        return result;
+    }
+    concat(str4);
+
+
+// Function scope  
+let sum = 54;               //Global scope          this can call anywhere, inside and outside the function
+function calsum(a,b){
+    let sum = a+b;          // Function scope       this can't call outside the function
+}
+
+calsum(1,2)
+
+
+
+// block ascope 
+{
+    let a = 25;
+    console.log(a); 
+}
+console.log(a);             // block ascope         can't call outside   
+
+
+for( i = 0; i<= 5 ; i++){
+    console.log(i);
+}
+console.log(i);              // block ascope         can't call outside  
+
+
+let age = 25;
+if(age >=18){
+    let str = 'adult';
+    console.log(str);
+}
+console.log(str);            // block ascope         can't call outside  
+
+
+// Laxical scope
+function outerfun(){
+    let x = 5;
+    let y = 6;
+    function innerfun(){
+        let a = 10;
+        let b = 8;
+
+        console.log(x);     // Child function can access parent function property 
+        console.log(y);
+        console.log(a);
+        console.log(b);
+    }
+    console.log(x);
+    console.log(y);
+    console.log(a);           // outside function can't call , parent function can't access their child function property
+    console.log(b);
+    innerfun(10);
+}
+
+// Practice question
+let greet = 'hello';                // global scope
+    function changegreet(){
+        let greet = 'namaste';      
+            console.log(greet);     // block scope
+
+            function innergreet(){  
+                console.log(greet); // lexical scope
+            }    
+            innergreet();
+    }
+    console.log(greet);
+    changegreet();
+
+
+// Function expresion
+
+let sum = function(a,b){
+    return a+b;
+}
+sum(1,2);
+
+let hello = function(){
+    console.log('hello');
+}
+hello = function(){
+    console.log('namaste');
+}
+
+// Higher order function
+
+function multiplegreet(func,count){
+    for(let i= 1; i<=count; i++){
+        func();
+    }
+}
+let greet = function(){
+    console.log('hello');
+}
+multiplegreet(greet,1000);
+
+
+// Higher order function // function factory
+
+function oddorevenFactory(request){
+    if(request == 'odd'){
+        let odd =function(n){
+        console.log(!(n%2==0));
+    }
+    return odd;
+    }else if(request == 'odd'){
+        let even =function(n){
+        console.log(n%2==0);
+    }
+    return even;
+}else{
+    console.log('wrong request');
+}
+}
+let request ='odd';
