928d419a2e6ef2e1ae5d471fae2c3d35f587c135
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-15 19:16:48 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 20/app.js
+++ JAVASCRIPT - 14 - 27/DAY - 20/app.js
@@ -81,22 +81,93 @@ const student1 = {
     prop : this,                     // global scope     here This not in any function it is global scope means rule will work like globle scope. 
 
     getName : function () {
-        return this.name ;
+        return this.name ;          // student
     },
 
     getMarks : () =>  {
         console.log(this);          // parent's scope -> window
         return this.marks ;
     },
-    getinfor : function(){
-        settimeout(() =>{
+    getinfo : function(){
+        settimeout(() =>{           // arrow function never change their rule   function call arrow function so parent's scope is == arrow scope
             console.log(this);      //student
         },2000)
     },
-    getinfor : function(){
-        settimeout(function (){
+    getinfo : function(){
+        settimeout(function (){     // function call by funtion but between setimeout is there so function change its rule and settimeout is windows so for function scope is windows 
             console.log(this);      //window
         },2000)
     },
 };
 
+
+
+//Practice
+// write an arrow funcion that return the square of a no. 'n';
+
+const square  = (n) => {
+    return n*n; 
+}
+
+const square1  = n => n*n; 
+square(5);
+
+// write function that print 'hello world ' 5 time at interval of 2s \
+
+const id1= setInterval(() =>{
+    console.log('hello world'); 
+},2000);
+
+const id2= setInterval(() =>{
+    return clearInterval(id1);         // stop setinterval after 5 sec
+},5000);
+
+
+// arrow function named arrayaverage that accept arrya of no. and return the average of those no.
+    
+const arrayaverage = (arr) => {
+let sum1 = 0;
+    for(let i = 0; i<= arr.length; i++){
+            let sum = arr[i] + sum1;
+    }
+    console.log(sum/3);
+}
+
+    let arr = [1,2,3,4,5,6];
+    arrayaverage(arr);
+
+
+// arrow function name is even tat take single no as argument and return if is even or not
+
+let num = 4;
+const arr5 = (num) => 
+    num%2==0;
+
+
+
+// find output
+const object ={
+    Message : 'Hello, world',
+
+    logMessage() {
+        console.log(this.Message);          //this == global object
+    }
+};
+setTimeout(object.logMessage, 1000);           // Window.message
+
+
+
+//find output
+
+let length = 4;
+function callback(){
+    console.log(this.length);
+} 
+const object5 = {
+    lenght: 5,
+    method5(callback){
+        callback();
+    },
+};
+
+object.method(callback,1,2);
