7d594301d813c269de390787fd749d5f6b525ffe
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-21 20:10:19 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 21/app.js
+++ JAVASCRIPT - 14 - 27/DAY - 21/app.js
@@ -161,3 +161,68 @@ function sum1(...args){
         sum + el;
     });
 }
+
+// Destructuring 
+
+let name = ['tony', 'bruce','peter','steve'];
+// let winner  = name [0];                      // winner is tony 
+// let runnerup  = name [1];                    // runnerup is bruce  
+// let secoundrunnerup  = name [2];             // secoundrunnerup is peter 
+let [winner ,runnerup, ...others ] = names;     // winner  = tony runneriup = peter and rest will come in ...others 
+
+// Destructuring object
+
+const student = {
+    name : "vatsal",
+    age : 14,
+    class : 14,
+    class : 9,
+    subject : ['hindi','english','math','science'],
+    username : "vatsalpatel1000",
+    password : 'vatsa'
+};
+
+let { username : user , password : secret , city : place = 'mumbai'} = student; 
+
+
+//Qs1. Square and sum the array elements using the arrow function and then find the average of the array.
+
+let num4 = [1,2,3,4,5]
+let num5 = num4.reduce((a,b) => {
+    return a+b;
+});
+let square = num5.map((a) => {
+        return a*a;
+});
+
+let avg = num5/num4.length;
+
+
+//Qs2. Create a new array using the map function whose each element is equal to the original element plus 5.
+let num7 = [1,2,3,4,5];
+
+let plus = num7.map((a)=>{
+    return a+5;
+});
+
+
+//Qs3. Create a new array whose elements are in uppercase of words present in the original array.
+
+let str = [ 'vatsal', 'bhavesh'];
+console.log(str.map((a) => a.toUpperCase()));
+
+
+//Qs4. Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of the additional arguments doubled.
+
+const doubleAndReturnArgs = (arr, ...args) =>[
+...arr, ...args.map((v)=> v*2)
+];
+
+
+// Qs5. Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+
+const mergeobject = (obj1,obj2) => ({
+    ...obj1 , ...obj2
+});
+
+mergeobject({ a:1, b: 2},{c:3,d: 4});
