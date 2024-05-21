d64d3c18b0739788b9ff32f33f045d0b6104a1fd
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-21 11:18:05 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 21/app.js
+++ JAVASCRIPT - 14 - 27/DAY - 21/app.js
@@ -12,7 +12,7 @@ arr.forEach((a) => {
     console.log(a);
 });
 
-//or   foreach for array function
+//or   foreach for object method
 let arr1 = [
     {
         name:'aman',
@@ -28,7 +28,7 @@ let arr1 = [
     },
 ];
 
-arr.forEach((a)=>{
+arr1.forEach((a)=>{
     console.log(a.marks);
 });
 
@@ -62,14 +62,14 @@ let ans = num2.filter((a)=>{
 });
 
 
-// every                        T + F = F
+// every                        T + F = F   LIke AND OPERATIOE
 [2,4,6].every((a)=>{
     a % 2 == 0;
 })
 
-//some                          T + F = T
-[2,4,6].some((b)=>{
-    a % 2 == 0;
+//some                          T + F = T   LIke OR OPERATIOE
+[2,4,6,1].some((b)=>{
+    b % 2 == 0;
 });
 
 // Reduce function
@@ -100,18 +100,64 @@ let result = nums.reduce ((max, el) => {
 })
 
 // practice question 
-// all no in put array are mu;tiple of 10 or not
+// all no in put array are mutiple of 10 or not
 let nums7 = [10,20,30,40,5]
 let ans7 =nums.every((el)=>{
     el % 10==0  
 });
 
 // find min np. in array
-let result6 = nums7.reduce ((max, el) => {
-    if(max< el){
+let result6 = nums7.reduce ((min, el) => {
+    if(min< el){
         return min;
     }
     else{
         return el;
     }
 });
+
+// Spread
+let arr2 = Math.min(1,2,3,0,4,1,3);
+console.log(arr2);
+
+arr3 = [1,2,3,0,4,1,3];
+console.log(...arr3);
+
+//spread with array litrals
+let odd = [1,3,5,7,9];
+let even = [2.4,6,8,10];
+let num3 = [...odd, ...even];   // concatination
+
+//spread with Object litrals
+
+const data = {
+    email : "vatsalpatel@gmail.com",
+    password:"abcd",
+};
+
+const datacopy = {...data, id : 123, contary : "india"}
+
+// arrary convert to object 
+let arrays = [1,2,3,4,5];   //val
+let obj = {...arr };        //obj -> key: value                 so for array index is the key here while convert from array to object
+
+
+// REST
+function sum(...args){      //arguments
+    for( let i =0 ; i<args.length;i++) {
+        console.log('you gave us :', args[i]);
+    }
+}
+
+function min(){
+    console.log(arguments);
+    console.log(arguments.length);
+    arguments.push(1);                  // argument is not an array so all array methos will not work with arguments
+}
+
+
+function sum1(...args){
+    return args.reduce((min,el)=>{
+        sum + el;
+    });
+}
