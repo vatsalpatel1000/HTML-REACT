cde8772b45ec4e5aeff7b441c4496517f8d07f22
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-22 22:55:49 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 22/DOM Starter Files/DOM Starter Files/DOM/app.js
+++ JAVASCRIPT - 14 - 27/DAY - 22/DOM Starter Files/DOM Starter Files/DOM/app.js
@@ -4,3 +4,17 @@ for(let i = 0 ; i<smallimages.length ; i++){
     smallimages[i].src = "/Assets/Spider-Man.png";
     console.log(`value of image no, ${i} is changed`);
 }
+
+
+document.getElementsByTagName("p")[0] = 'abc';
+
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#description'));
+console.log(document.querySelector('.oldImg'));
+console.log(document.querySelector("div a "));
+console.log(document.querySelectorAll("div a "));
+
+
+let para = document.querySelector('p');
+para.innerText='abc<b>bbbbb</b>'
+para.innerHTML='abc<b>bbbbb</b>'
