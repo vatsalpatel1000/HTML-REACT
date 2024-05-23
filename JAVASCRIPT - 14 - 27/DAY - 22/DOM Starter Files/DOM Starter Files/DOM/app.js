5bb5cca827051296d090ee48f0caee3f9f3e14e1
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-24 00:06:37 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 22/DOM Starter Files/DOM Starter Files/DOM/app.js
+++ JAVASCRIPT - 14 - 27/DAY - 22/DOM Starter Files/DOM Starter Files/DOM/app.js
@@ -16,5 +16,29 @@ console.log(document.querySelectorAll("div a "));
 
 
 let para = document.querySelector('p');
-para.innerText='abc<b>bbbbb</b>'
-para.innerHTML='abc<b>bbbbb</b>'
+para.innerText='abc<b>bbbbb</b>'            // screen text on display
+para.innerHTML='abc<b>bbbbb</b>'            // Html index page 
+para.textContent = ;                        // show all display hide data like 
+
+
+// Manipulation attributes
+
+let img = document.querySelector("mainImg");
+img.getAttribute(id);
+img.setAttribute("id", 'spiderman')
+
+
+// Manipulate style
+let links = document.querySelectorAll('.pubInfo a') ;
+
+for(let i=0; i<links.length;i++ ){
+    links[i].style.color= 'yellow';
+}
+
+//classlist property
+
+list.heading = document.querySelector(h1);
+heading.classlist.add('green');                 //create class for h1 name with green
+heading.classlist.remove('green');              //delete class for h1 name of green
+heading.classlist.contains('green');            //check class name of green fi its there than will give true
+heading.classlist.toggle('green');              //if green classname is already create than it will delete and if its not there than create it with green classname 
