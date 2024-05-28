019a8cee7bf63e34bf16c33fc9e61cb596c1d727
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-05-28 21:15:13 +0530

    add Html css folder

--- JAVASCRIPT - 14 - 27/DAY - 26/app.js
+++ JAVASCRIPT - 14 - 27/DAY - 26/app.js
@@ -1,30 +1,22 @@
 // Async Function
-    
-h1 =document.querySelector('h1');
 
-function changeColor(color,delay){
-    return  new Promise((resolved,rejected)=>{
-        setTimeout(()=>{
-            h1.style.color = color;
-        },delay);
-    })
+async function greet(){
+    return "hello world";       //by default return a promise
 }
 
-changeColor("red",1000)
-.then(()=>{
-    console.log("red color");
-    return changeColor("green",1000)
-})
-.then(()=>{
-        console.log("yellow  color");
-        return changeColor("yellow",1000)
-})
-.then((result)=>{
-    console.log("blue color");
-    return changeColor("Blue",1000);
-    console.log('result of promise:', result);
-})
-.catch((error)=>{
-    console.log("promise was rejected");
-    console.log("error of promises:", error);
-});
+let hello = async () => {};     //arrow function returns a promise 
+
+
+async function greet1(){
+    throw "weak connection";
+    return "hello";
+}
+
+greet1()
+    .then((result)=>{
+        console.log("promise was resolved");
+        console.log("result was :", result);
+    })
+    .catch((err)=>{
+        console.log("promise was rejected with err: ",err);
+    });
