042c76d844855b1b58f4012cf885f3bb35f75886
vatsalpatel1000 <vatsalpatel1000@gmail.com>
2024-06-09 06:14:31 +0530

    add new Message

--- 5_Node - 30/DAY - 32/EJS/index.js
+++ 5_Node - 30/DAY - 32/EJS/index.js
@@ -1,8 +1,39 @@
 const express = require ("express");
 const app = express();
+const path = require ("path");
 
-const port =3000;
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, "/public/css")));                       // redirect to public folder for css  // middleware
+app.use(express.static(path.join(__dirname, "/public/js")));  
 
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`);
+})
+
+// app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "/views"))         // give the path of view folder for connect ejs file with index file while run program from outside
+
+app.get("/",(req,res) => {
+    res.render("home.ejs");                             // RENDER will find view folder and redirect home.ejs 
+})
+
+app.get("/rolldice",(req,res)=>{                        // route 
+    let diceVel = Math.floor(Math.random()*6)+1         // database data pass
+    res.render("rolldice.ejs",{diceVel});               // data pass to file 
+})
+
+// app.get("/ig/:username",(req,res)=>{                    // route with parameter
+//     const followers = ['vats','bhav','nirbh'];          // pass array 
+//     let { username } = req.params;
+//     console.log("username");
+//     res.render("instagram.ejs" , {username , followers});           
+// })
+
+app.get("/ig/:username",(req,res)=>{                        // get request by user with route and parameter
+    let { username } = req.params;                          // get parameter
+    let instadata = require('./data.json');                 // call data.json file 
+    const data = instadata[username];                       // call user parameter data in data.json file 
+    console.log(data);
+    res.render('instagram.ejs',{ data });                   // parameter data send to ejs file to print  
 })
---
+++ 5_Node - 30/DAY - 32/EJS/public/js/app.js
@@ -0,0 +1,6 @@
+btns = document.querySelectorAll("button");
+
+for(btn of brns){
+btn.addeventlistner("click",()=>{
+    console.log('button is clicked');
+})}
