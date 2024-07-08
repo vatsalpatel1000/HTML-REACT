a9fc620bdac508a70d44dd1f1bf8c03093c1c099
vatsalpatel1000 <vatsalpatel1000>
2024-07-09 01:47:22 +0530

    add new Message

--- 5_Node - 30/DAY - 40/NODE WITH MONGODB/index.js
+++ 5_Node - 30/DAY - 40/NODE WITH MONGODB/index.js
@@ -1,30 +1,124 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const Chat = require('./models/chat.js')
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose')
+// const Chat = require('./models/chat.js')
 // const { faker } = require("@faker-js/faker");
-const path = require("path");
+// const path = require("path");
 // const methodOverride = require("method-override");
 // const { v4: uuidv4 } = require("uuid");
-const port = 8080
+// const port = 8080
 
 
-//app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
+// app.use(express.urlencoded({ extended: true }));
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "/views"));
+
+
+
+
+// app.get('/chats', async (req,res)=>{
+//     let chats = await Chat.find();
+//     console.log(chats);
+//     req.send("working");
+// })
+
+// app.get('/', (req, res) => 
+//     res.send('Hello World!'))
+
+// app.listen(port, () =>
+//     console.log(`Example app listening on port ${port}!`))
+
+// app.get('/',(req,res)=>{
+//     res.send("root is working");  
+// })
+
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const Chat = require('./models/chat.js');
+const methodOverride = require("method-override");
+
+
+const app = express();
+const port = 8080;
+
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
 
+mongoose.connect('mongodb://127.0.0.1:27017/whatsapp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connection successful"))
+    .catch((err) => console.log(err));
 
+//Index Route 
+app.get('/chats', async (req, res) => {
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+});
 
-
-
-
-app.get('/', (req, res) => 
-    res.send('Hello World!'))
-
-app.listen(port, () =>
-     console.log(`Example app listening on port ${port}!`))
-
-app.get('/',(req,res)=>{
-    
+//New Route
+app.get('/chats/new',(req,res)=>{
+    res.render("new.ejs");
 })
+
+//Create Route
+app.post('/chats',(req,res)=>{
+    let {from , to , msg }=req.body
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        msg : msg,
+        created_at : new Date()
+    });
+    newChat.save()      // save to DBS
+    .then((res)=>{
+        console.log("chat was saved")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+    res.redirect("/chats");
+});
+
+//Edit Route
+app.get('/chats/:id/edit',async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{ chat });
+})
+
+//Update Route
+app.put('/chats/:id',async(req,res)=>{
+    let {id} = req.params;
+    let {msg : newmsg} = req.body;
+    let updateChat = await Chat.findByIdAndUpdate(id,{msg : newmsg},{runValidators:true},{new : true})
+    console.log("updated chat");
+    res.redirect("/chats");
+})
+
+//DELETE Route
+app.delete('/chats/:id', async(req,res)=>{
+    let{ id } = req.params;
+    let DeletedcChat = await Chat.findByIdAndDelete(id);
+    console.log(DeletedcChat);
+    res.redirect("/chats");
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+app.get('/', (req, res) => {
+    res.send("root is working");
+});
--- 5_Node - 30/DAY - 40/NODE WITH MONGODB/init.js
+++ 5_Node - 30/DAY - 40/NODE WITH MONGODB/init.js
@@ -1,54 +1,117 @@
-const mongoose = require('mongoose')
-const Chat = require('./models/chat.js')
+// const mongoose = require('mongoose')
+// const Chat = require('./models/chat.js')
+
+// main()
+//     .then((res)=>{
+//     console.log(res);
+//     console.log("connection successfull");
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     });
+
+// async function main() {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+// }
+
+// // let Chat1 = new Chat({
+// //     from : "neha",
+// //     to : "priya",
+// //     msg : 'send me your exam sheets',
+// //     created_at : new Date()
+// // });
+
+// const allChats =[
+//     {
+//     from : 'neha',
+//     to : "priya",
+//     msg : 'send me your exam sheets',
+//     created_at : new Date()
+//     },
+//     {
+//     from : 'shee',
+//     to : "shee",
+//     msg : 'send me your exam sheets',
+//     created_at : new Date()
+//     },
+//     {
+//     from : 'hee',
+//     to : "hee",
+//     msg : 'send me your exam sheets',
+//     created_at : new Date()
+//     },
+//     {
+//     from : 'him',
+//     to : "him",
+//     msg : 'send me your exam sheets',
+//     created_at : new Date()
+//     },
+//     {
+//     from : 'they',
+//     to : "they",
+//     msg : 'send me your exam sheets',
+//     created_at : new Date()
+//     },
+
+// ];
+
+// Chat.insertMany(allChats);
+
+// Chat1.save().then((res)=>{
+//     console.log(res);
+// })
+
+
+const mongoose = require('mongoose');
+const Chat = require('./models/chat.js');
 
 main()
-    .then((res)=>{
-    console.log(res);
-    console.log("connection successfull");
+    .then(() => {
+        console.log("Connection successful");
+        seedDB();
     })
     .catch((err) => {
-        console.log(err)
+        console.log(err);
     });
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp', { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
-const allChats =[
+const allChats = [
     {
-    from : 'neha',
-    to : "priya",
-    msg : 'send me your exam sheets',
-    created_at : new Date()
+        from: 'neha',
+        to: "priya",
+        msg: 'send me your exam sheets',
+        created_at: new Date()
     },
     {
-        from : 'neha',
-        to : "priya",
-        msg : 'send me your exam sheets',
-        created_at : new Date()
-        },
-        {
-            from : 'neha',
-            to : "priya",
-            msg : 'send me your exam sheets',
-            created_at : new Date()
-            },
-            {
-                from : 'neha',
-                to : "priya",
-                msg : 'send me your exam sheets',
-                created_at : new Date()
-                },
-                {
-                    from : 'neha',
-                    to : "priya",
-                    msg : 'send me your exam sheets',
-                    created_at : new Date()
-                    },
+        from: 'shee',
+        to: "shee",
+        msg: 'send me your exam sheets',
+        created_at: new Date()
+    },
+    {
+        from: 'hee',
+        to: "hee",
+        msg: 'send me your exam sheets',
+        created_at: new Date()
+    },
+    {
+        from: 'him',
+        to: "him",
+        msg: 'send me your exam sheets',
+        created_at: new Date()
+    },
+    {
+        from: 'they',
+        to: "they",
+        msg: 'send me your exam sheets',
+        created_at: new Date()
+    }
 ];
 
-// let Chat1 = new Chat({
-    Chat.insertMany(allChats);
+Chat.insertMany(allChats);
 
 Chat1.save().then((res)=>{
     console.log(res);
